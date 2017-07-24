bcbb1283909dcd87605e00ad6493dcc357fb50db
Shem Boduch <pbboduch@chicagobooth.edu>
2017-07-24 16:41:46 +0000

    msm debug v1

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,47 +1,53 @@
 class ActorsController < ApplicationController
   def index
     @actors = Actor.all
+    render("actors/index.html.erb")
   end
 
   def show
     @actor = Actor.find(params[:id])
+    render("actors/show.html.erb")
   end
 
   def new_form
+    render("actors/new_form.html.erb")
   end
 
   def create_row
-    @actor = Actor.new
-    @actor.dob = params[:dob]
-    @actor.name = params[:name]
-    @actor.bio = params[:bio]
-    @actor.image_url = params[:image_url]
+    actor = Actor.new
+    
+    actor = Actor.new
+    actor.dob = params[:dob]
+    actor.name = params[:name]
+    actor.bio = params[:bio]
+    actor.image_url = params[:image_url]
 
-    @actor.save
+    actor.save
 
     render("show")
   end
 
   def edit_form
     @actor = Actor.find(params[:id])
+    render("actors/edit_form.html.erb")
   end
 
   def update_row
-    @actor = Actor.find(params[:id])
+    actor = Actor.find(params[:id])
 
-    @actor.dob = params[:dob]
-    @actor.name = params[:name]
-    @actor.bio = params[:bio]
-    @actor.image_url = params[:image_url]
+    actor.dob = params[:dob]
+    actor.name = params[:name]
+    actor.bio = params[:bio]
+    actor.image_url = params[:image_url]
 
-    @actor.save
+    actor.save
 
     render("show")
   end
 
   def destroy
-    @actor = Actor.find(params[:id])
-
-    @actor.destroy
+    actor = Actor.find(params[:id])
+    actor.destroy
+    redirect_to("/actors")
   end
 end
